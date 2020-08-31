a7c86d71044ba2444f67edd182f3c0fbead6d37a
B-Saidy <baijaw93@yahoo.com>
2020-08-31 18:34:13 +0800

    changed Debug value

--- btrs/settings.py
+++ btrs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!m4f%)!qg$u=v2l5ky12ua%@($!3u^!xcu@+3!@8y_6wrg*o9t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['busticket-reserve.herokuapp.com', 'localhost']
 
